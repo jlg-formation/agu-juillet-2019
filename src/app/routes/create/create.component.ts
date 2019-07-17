fe92aa42026562a38fedcf968630e5fdeabe6ffc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-07-17 11:35:15 +0200

    create form

--- src/app/routes/create/create.component.ts
+++ src/app/routes/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
+  f = new FormGroup({});
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submit() {
+    console.log('submit');
   }
 
 }
